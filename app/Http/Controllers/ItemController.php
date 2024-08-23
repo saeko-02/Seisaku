a41f4493265da9279e0032d9176c4b4d0b2335d5
saeko-02 <163119513+saeko-02@users.noreply.github.com>
2024-08-23 20:03:38 +0900

    test

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -57,4 +57,50 @@ class ItemController extends Controller
 
         return view('item.add');
     }
+
+    // 編集画面
+    public function edit($id)
+    {
+        $item = Item::where('id','=',$id)->first();
+        return view('item.edit')->with([
+            'item'=>$item
+        ]);
+
+    }
+
+    public function update(Request $request,$id)
+    {
+        // dd($request);
+
+        $item = Item::find($id);
+
+            if (!$item) {
+    return redirect('items/')->withErrors(['error' => '指定された項目が見つかりませんでした。']);
+}
+
+        $validatedData=$request->validate([
+            'name' => 'required|max:100',
+            'type' => 'required',
+            'detail' => 'nullable|max:255',
+
+        ]);
+
+        $item->update($validatedData);
+        // $item->name = $request->name;
+        // $item->type = $request->type;
+
+        $item->save();
+
+        return redirect('items/')->with('success','項目が正常に更新されました。');
+    }
+
+public function destroy($id)
+{
+    $item = Item::find($id);
+    $item->delete();
+    // 削除したら一覧画面にリダイレクト
+    return redirect('/items')->with('success', 'アイテムが削除されました');
+
+        }
+
 }
